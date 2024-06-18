e122bfe52f8b948a307b4a21e6400a38922344ce
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-06-18 13:00:12 -0300

    construction of game logic

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+const size = 30
+
+const snake = [
+	{x:200, y:200},
+	{x:230, y:200}
+]
+
+let direction
+
+const drawSnake = () => {
+    ctx.fillStyle = "#ddd"
+    
+    snake.forEach((position, index) => {
+        
+        if (index == snake.length -1) {
+            ctx.fillStyle = "blue"
+        }
+
+        ctx.fillRect(position.x, position.y, size, size)
+    })
+}
+
+const moveSnake = () => {
+
+    if(!direction) return
+
+    const head = snake[snake.length - 1]
+
+    if (direction == "rigth" ){
+        snake.push({ x: head.x + size, y: head.y })
+    }
+
+    if (direction == "left" ){
+        snake.push({ x: head.x - size, y: head.y })
+    }
+    
+    if (direction == "down" ){
+        snake.push({ x: head.x, y: head.y + size })
+    }
+
+    if (direction == "up" ){
+        snake.push({ x: head.x, y: head.y - size })
+    }
+}
+
+drawSnake()
